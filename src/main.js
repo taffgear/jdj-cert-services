da4812608b4d51367b9e8e7a6f92e92e90703db1
markhorsman <mark.horsman@tam-one.nl>
2020-02-17 17:10:50 +0100

    Added existing files to project

---
+++ src/main.js
@@ -0,0 +1,61 @@
+const vision = require('@google-cloud/vision').v1;
+const fs = require('fs');
+
+/**
+ * Perform batch file annotation
+ *
+ * @param filePath {string} Path to local pdf file, e.g. /path/document.pdf
+ */
+async function PDFToText(filePath) {
+	const client = new vision.ImageAnnotatorClient();
+
+	// Supported mime_type: application/pdf, image/tiff, image/gif
+	// The service can process up to 5 pages per document file
+	const pages = [1, 2];
+	const requestsElement = {
+		inputConfig: {
+			mimeType: 'application/pdf',
+			content: fs.readFileSync(filePath).toString('base64')
+		},
+		features: [{
+			type: 'DOCUMENT_TEXT_DETECTION'
+		}],
+		pages
+	};
+
+	const requests = [ requestsElement ];
+	
+	try {
+		const responses = await client.batchAnnotateFiles({ requests: requests })
+		return responses[0].responses[0].responses
+
+	} catch (e) {
+		console.error(e);
+	}	
+}
+
+async function main() {
+	const file = process.env.FILE || null;
+	const test = process.env.TEST || false; // provide url to txt file
+	let txt;
+
+	if (!file && !test) {
+		console.warning('No PDF input file given');
+		return;
+	}
+
+	if (!test) {
+		const pages = await PDFToText(file);
+
+		for (const r of pages) {
+			txt += r.fullTextAnnotation.text;
+		}
+	} else {
+		txt = fs.readFileSync(test).toString();
+	}
+
+	console.log(txt);
+
+}
+
+main();
